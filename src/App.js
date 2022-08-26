6ba4081768c25814fd85836fd49cb91bc14a436c
serhatbek <serhat.bekk@gmail.com>
2022-08-26 12:51:07 +0300

    project setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
-  return <div className='App'>navbar</div>;
+  return (
+    <div className='App'>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from 'react';
+import { FaBars, FaTwitter } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from './logo.svg';
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
